aa1f12f2f8cb8d0038977fc45e291640150cd393
severnsc <severnsc@gmail.com>
2017-06-19 13:37:38 -0400

    Output product name on new screen when barcode successfully scanned

--- productPage.js
+++ productPage.js
@@ -9,14 +9,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
-  }
+  },
+  header: {
+    fontSize: 48,
+  },
 })
 
 export default class ProductPage extends Component{
   render(){
     return(
       <View style={styles.container}>
-        <Text>
+        <Text style={styles.header}>
           {this.props.productName}
         </Text>
       </View>
